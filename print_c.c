59101e2e07545bc65734033292b3b456491775ea
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-10-17 15:50:33 -0700

    char

---
+++ print_c.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+/**
+ * printf_char - printf character
+ * @val: argment
+ * Return: 0
+ */
+int printf_char(va_list val)
+{
+	char str;
+
+	str = va_arg(val, int);
+	_putchar(str);
+	return (0);
+}

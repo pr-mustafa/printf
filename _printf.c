5b6d86616f6c33c2824e3561d517614ec3d50c5f
ahmedsayed940 <ahmeds.space940@gmail.com>
2023-10-20 14:53:10 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -1,41 +1,36 @@
 #include "main.h"
+
 /**
- * _printf - print acorden to foramt
- * @format: format of argmeant
- * @...: argments
- * Return: len
- *
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
  */
 int _printf(const char *format, ...)
 {
-	match m[] = {
-		{"%c", printf_char}, {"%s", print_string}, {"%%", print_37}, 
-		{"%d", print_dec}, {"%i", print_int}
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
 	};
-	va_list args;
-	int i = 0, len = 0, j;
+	va_list arg_list;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
-here:
-	while (format[i] == '\0')
-	{
-		j = 13;
-		while (j >= 0)
-		{
-			if ((m[j].id[0] == format[i]) && m[j].id[1] == format[i + 1])
-			{
-				len = len + m[j].f(args);
-				i = i + 2;
-				goto here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		i++;
-		len++;
-	}
-	va_end(args);
-	return (len);
+
+	va_start(arg_list, format);
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
